464e078b08f7fd94acd1230e2e29bb05c6945688
Lorejimenez1 <38303482+Lorejimenez1@users.noreply.github.com>
2018-12-01 01:49:00 -0700

    Update form.js
    code cleanup

--- src/components/form.js
+++ src/components/form.js
@@ -1,45 +1,34 @@
 import React from 'react';
-import './form.css';
+import './list.css';
 
-export default class AddForm extends React.Component {
-
-    onSubmit(event) {
-        event.preventDefault();
-        const text = this.textInput.value.trim();
-        const pet = this.petInput.value.charAt(0).toLowerCase() + this.petInput.value.slice(1);
-        const breed = this.breedInput.value.charAt(0).toUpperCase() + this.breedInput.value.slice(1); 
-        if (text && pet && breed && this.props.onAdd) {
-            this.props.onAdd(text, pet, breed);
-        }
-        else if (text && pet && this.props.onAdd) {
-            const breed=""
-            this.props.onAdd(text, pet, breed);
-        }
-
-        this.textInput.value = '';
-        this.petInput.value = '';
-        this.breedInput.value = ''
-    }
+export default class List extends React.Component {
 
     render() {
+         const description = this.props.description.$t.slice(0, 720);
+         
+         return (
+        
+                    <ul className="container" id="container">
+                            <img className=" img item" src={this.props.media.photos.photo[3].$t} alt="A surprising fig" />
+                    
+                            <li className="item">
+                                Meet {this.props.name.$t}/{this.props.breeds.breed.$t}    
+                            </li>
+                            <li className="item">    
+                                Contact: {this.props.contact.phone.$t} or {this.props.contact.email.$t}
+                            </li>
+                            <li className="item">  
+                                at shelter {this.props.shelterId.$t} in {this.props.contact.city.$t}
+                             </li>   
+                             
+                            <li className="item"> 
+                                 {this.props.size.$t} {this.props.age.$t}
+                            </li>
 
-        return (
-            <form className="add-form" onSubmit={(e) => this.onSubmit(e)}>
-                <label>
-                <input type="text" ref={input => this.textInput = input} required/>
-                 <div className="label-text">Zipcode</div>
-                </label>
-                <label> 
-                <input type="text" placeholder="I.E: dog, cat, or bird" ref={input => this.petInput = input} required/>
-                <div className="label-text">Animal</div>
-                </label>
-                <label> 
-                <input type="text" placeholder="Any" ref={input => this.breedInput = input} />
-                <div className="label-text">Breed</div>
-                </label>
-                <button  formNoValidate="formNoValidate" type="submit">Submit</button>
-                
-            </form>
-        );
+                            <li className="item"> 
+                                 {description}
+                            </li>
+                    </ul>    
+               );         
     }
 }
