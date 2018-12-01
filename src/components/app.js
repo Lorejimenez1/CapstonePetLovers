384507bc569d90cfb0419b261354604741d46af6
Lorejimenez1 <38303482+Lorejimenez1@users.noreply.github.com>
2018-12-01 01:46:05 -0700

    Update app.js
    Update component for loading spinner and add more error handling logic.

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,29 @@
-import React from 'react';
+imort React from 'react';
 
 import List from './list';
 import AddForm from './form';
+import Spinner from 'react-spinkit';
 
 import './app.css';
 
 import {connect} from 'react-redux';
-import { fetchPets} from '../actions';
+import { searchPets} from '../actions';
 
 export  class App extends React.Component {
-   search(location, animal, breed) {
-        this.props.dispatch(fetchPets(location, animal, breed));
-    } 
+    renderResults() {
+        if (this.props.loading) {
+            return <Spinner name="cube-grid" color="purple" noFadeIn />;
+        }
+
+        else if (this.props.error) {
+            return <h1>{this.props.error} check Zipcode or animal</h1>;
+        }
+        else if(this.props.pets === undefined) {
+            return <h1>Check to make sure breed is spelled corecctly.We may not have that breed in our database. If you wish to search by all leave breed field empty</h1>;
+
+        }
     
-   render() {
-        let body;
-       
-        const lists = this.props.pets.map((list, index) => (
+             const lists = this.props.pets.map((list, index) => (
                 <div key={index}>
                     <List
                         index={index}
@@ -24,36 +31,40 @@ export  class App extends React.Component {
                     />    
                 </div>
             ));
-            body = (
-                <div className="row">
-                    {lists}
-                </div>
-            );
+        return(
+            <div className="row">
+                {lists}
+            </div>    
+        )};    
         
+    
 
+    search(location, animal, breed) {
+        if(breed === undefined) { 
+        const breed=""
+    }
+            this.props.dispatch(searchPets(location, animal, breed));
+    } 
+    
+    render() {
+       
         return (
-        
-            
+             
             <div>
                 <img className="logo" src="https://techflourish.com/images/blue-cat-paws-clipart-6.png" alt="animal logo"/>
-                <h1>Find your new Pet</h1>   
+                <h1>Find animals that are up for adoption now. Enter your zipcode and animal. Animal options include dog, cat and bird. <br /> Optionally you may search by breed. If you choose not to search by breed just leave the field as Any an hit submit.</h1>   
                 <section className="form-column">
                     <AddForm
                     type="card"
                     onAdd={(text, pet, breed) => this.search(text, pet, breed)}
                 />
-                </section>    
-                {body}
+                </section> 
+                   
+                {this.renderResults()}
+                
             </div>
         
         );
     
     }
 }
-
-const mapStateToProps = state => ({
-    pets: state.pets,
-});
-
-export default connect(mapStateToProps)(App);
-
