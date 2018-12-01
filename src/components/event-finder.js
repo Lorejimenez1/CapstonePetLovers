ae9f7dfdacce2cee133142ebcafa6b6067819e40
Lorejimenez1 <38303482+Lorejimenez1@users.noreply.github.com>
2018-12-01 01:46:42 -0700

    Update event-finder.js
    Update component for loading spinner and add more error handling logic

--- src/components/event-finder.js
+++ src/components/event-finder.js
@@ -4,20 +4,20 @@ import { fetchEvents} from '../actions';
 
 import Events from './events';
 import EventForm from './event-form';
-
+import Spinner from 'react-spinkit';
 
 import './app.css';
 
 export class EventFinder extends React.Component {
-
-   search(location) {
-        this.props.dispatch(fetchEvents(location));
-    }
-   
-   render() {
-        let body;
-   
-        const lists = this.props.array.map((list, index) => (
+    renderResults() {
+        if(this.props.loading) {
+            return <Spinner name="cube-grid" color="purple" fadeIn='none' />;
+        }
+        else if(this.props.error) {
+            return <h1>{this.props.error}</h1>
+        }
+        else if(this.props.array.length > 0) {
+           const lists = this.props.array.map((list, index) => (
                 <div  key={index}>
                     <Events
                         index={index}
@@ -25,28 +25,34 @@ export class EventFinder extends React.Component {
                     />    
                 </div>
             ));
-            body = 
-            (
-            <div className="row">
-                {lists}
-            </div>
-            )
-                
-            ;
+            return(
+                <div className="row">
+                    {lists}
+                </div>
+            )};
+        
+    
+         
+    };
+   search(location) {
+        this.props.dispatch(fetchEvents(location));
+    }
+   
+   render() {
         
         return (
-        
-            
+         
             <div>
                 <img className="logo" src="https://techflourish.com/images/blue-cat-paws-clipart-6.png" alt="animals and paws logo"/>
-                <h1>Find events nationwide</h1>   
+                <h1>Find events like pet adoptions and fundraisers to help pets.
+                For example enter california and receive realtime events and information based in that state in the U.S<br />(note: state must be in fullname format I.E Arizona) .</h1>  
                 <section className="form-column">
                     <EventForm
                     type="card"
                     onAdd={(text) => this.search(text)}
                 />
                 </section>    
-                {body}
+                {this.renderResults()}
             </div>
         
         );
@@ -56,7 +62,10 @@ export class EventFinder extends React.Component {
 
 const mapStateToProps = state => ({
     array: state.array,
+    error: state.error,
+    loading: state.loading
 });
 
 export default connect(mapStateToProps)(EventFinder);
+
 
